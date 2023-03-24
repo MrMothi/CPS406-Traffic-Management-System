458cb7b9814d80d305dd359da678d3e9cabcf454
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-23 22:49:12 -0400

    Update PublicTransit.py

--- PublicTransit.py
+++ PublicTransit.py
@@ -1,2 +1,11 @@
-class PublicTransit:
-    pass
+class PublicTransit(Vehicle):
+    def __init__(self, operational, inIntersection, stopped, name, speed, type, plate, inBusLane):
+        super().__init__(operational, inIntersection, stopped, name, speed, type, plate)
+        self.inBusLane = inBusLane
+        self.operational = operational
+        self.inIntersection = inIntersection
+        self.stopped = stopped
+        self.name = name
+        self.speed = speed
+        self.type = type
+        self.plate = plate
