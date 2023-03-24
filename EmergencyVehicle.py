cb11100b5e684870ab6674659b3755f2c89392dc
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-23 22:56:27 -0400

    Update EmergencyVehicle.py

--- EmergencyVehicle.py
+++ EmergencyVehicle.py
@@ -1,2 +1,13 @@
-class EmergencyVehicle:
-    pass
+class EmergencyVehicle(Vehicle):
+    def __init__(self, operational, inIntersection, stopped, name, speed, type, plate, onScene, emergencyType, emergencySignal):
+        super().__init__(operational, inIntersection, stopped, name, speed, type, plate)
+        self.operational = operational
+        self.inIntersection = inIntersection
+        self.stopped = stopped
+        self.name = name
+        self.speed = speed
+        self.type = type
+        self.plate = plate
+        self.onScene = onScene
+        self.emergencyType = emergencyType
+        self.emergencySignal = emergencySignal
