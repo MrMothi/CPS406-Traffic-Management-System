d954cefb246eab3b03697c5093923beacbcd92a2
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-23 22:23:20 -0400

    Update Vehicle.py

--- Vehicle.py
+++ Vehicle.py
@@ -1,2 +1,9 @@
 class Vehicle:
-    pass
+    def __init__(self, operational, inIntersection, stopped, name, speed, type, plate ):
+        self.operational = True
+        self.inIntersection = True
+        self.stopped = True
+        self.name = "" #car id
+        self.speed = 0
+        self.type = "" #can be either car, public transport, or emergency
+        self.plate = "" 
