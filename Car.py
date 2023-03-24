99d3ab9faabb2dc0e1645f6a371d6c4ea0e42eec
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-23 22:25:50 -0400

    Update Car.py

--- Car.py
+++ Car.py
@@ -1,3 +1,4 @@
-class Car:
-    def __init__(self, car_type):
-        self.carType = car_type
+class Car(Vehicle):
+    def __init__(self, operational, inIntersection, stopped, name, speed, type, plate, car_type):
+        super().__init__(operational, inIntersection, stopped, name, speed, type, plate)
+        self.car_type = car_type
