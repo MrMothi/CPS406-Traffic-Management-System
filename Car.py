fb46cdc515dcdbf957b930634efc8c487c8f16ee
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-23 22:50:59 -0400

    Update Car.py

--- Car.py
+++ Car.py
@@ -2,3 +2,10 @@ class Car(Vehicle):
     def __init__(self, operational, inIntersection, stopped, name, speed, type, plate, car_type):
         super().__init__(operational, inIntersection, stopped, name, speed, type, plate)
         self.carType = car_type
+        self.operational = operational
+        self.inIntersection = inIntersection
+        self.stopped = stopped
+        self.name = name
+        self.speed = speed
+        self.type = type
+        self.plate = plate
