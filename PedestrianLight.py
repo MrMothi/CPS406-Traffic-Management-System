a6e8da14f8ec1b73216fd9c661312e73436e203b
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-24 23:28:22 -0400

    Update PedestrianLight.py

--- PedestrianLight.py
+++ PedestrianLight.py
@@ -1,2 +1,7 @@
 class PedestrianLight:
-    pass
+    def __init__(self, operational, hasAudibleSignal, signalColour, timeToWalk, walkTiming):
+        self.operational = operational
+        self.hasAudibleSignal = hasAudibleSignal #bool
+        self.signalColour = signalColour #string
+        self.timeToWalk = timeToWalk #int
+        self.walkTiming = walkTiming #int
