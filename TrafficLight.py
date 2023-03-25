5fd773d50d6448f7870ee6afa28fc5230f2f5a21
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-24 23:15:04 -0400

    Update TrafficLight.py

--- TrafficLight.py
+++ TrafficLight.py
@@ -1,2 +1,7 @@
 class TrafficLight:
-    pass
+    def __init__(self, operational, signalColour, timeRed, timeGreen, timeYellow):
+        self.operational = operational #bool
+        self.signalColour = signalColour #string
+        self.timeRed = timeRed #int
+        self.timeGreen = timeGreen #int
+        self.timeYellow = timeYellow #int
