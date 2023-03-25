3467140959bf702448fa99f0d17121254e87a8bb
MrMothi <90167278+MrMothi@users.noreply.github.com>
2023-03-25 03:03:23 -0400

    changed traffic system

--- TrafficSystem.py
+++ TrafficSystem.py
@@ -1,5 +1,12 @@
 class TrafficSystem:
-    def __init__(self, intersectionName, authUsers, authPass):
-        self.intersectionName = intersectionName #string
-        self.authUsers = authUsers #arraylist
-        self.authPass = authPass #arraylist
+    intersectionName = "MetroFloRoad"
+    userInfoDict = {
+        "Admin1" : "Pa55word",
+        "Admin2" : "traffic"
+    }
+
+
+    #implement login system here, backend for the frontend
+
+
+#not object class
