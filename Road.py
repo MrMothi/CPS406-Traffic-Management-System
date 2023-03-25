c462c9f3494e9bc2bf9190c48f202eaa713335f9
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-24 23:08:23 -0400

    Update Road.py

--- Road.py
+++ Road.py
@@ -1,2 +1,8 @@
 class Road:
-    pass
+    def __init__(self, operational, hasBusLane, hasSpeedCameras, sideWalks, vehiclesInLane1, vehiclesInLane2):
+        self.operational = operational
+        self.hasBusLane = hasBusLane
+        self.hasSpeedCameras = hasSpeedCameras
+        self.sideWalks = sideWalks
+        self.vehiclesInLane1 = vehiclesInLane1
+        self.vehiclesInLane2 = vehiclesInLane2
