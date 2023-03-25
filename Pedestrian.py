6fd37e4fbf3d8d10be33656e14037d53ce291f34
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-25 17:08:25 -0400

    Update Pedestrian.py

--- Pedestrian.py
+++ Pedestrian.py
@@ -5,10 +5,10 @@ class Pedestrian:
         self.name = name #bool
 
     def crossStreet(self): #changing it from destination:Sidewalk bc we are no longer following pedestrian after crossing - simplifying things
-        return bool
+        return True
     
     def requestCrossSignal(self):
-        return bool
+        return True
     
     def checkCrossLight(self, destination: Sidewalk): #leaving destination:sidwalk for now bc we prolly gonna be checking which sidewalk they on before crossing
         return ""
