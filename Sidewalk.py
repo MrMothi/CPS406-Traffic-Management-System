a3e56671c935164ccd153c334d6cddaa5b7fdaec
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-24 23:19:00 -0400

    Update Sidewalk.py

--- Sidewalk.py
+++ Sidewalk.py
@@ -1,2 +1,5 @@
-class Sidewalk:
-    pass
+class SideWalk:
+    def __init__(self, operational, countPedestrians, pedestrians):
+        self.operational = operational #bool
+        self.countPedestrians = countPedestrians #int
+        self.pedestrians = pedestrians #arraylist queue pedestrian
