a56043fbfbb7b6fce4ae443ac874c971dd7ba8e6
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2023-03-26 02:34:02 -0400

    Add files via upload

---
+++ AdminBackend.py
@@ -0,0 +1,32 @@
+import Intersection
+import TrafficLight
+from Vehicle import EmergencyVehicle
+
+class AuthorizedUser:
+    def __init__(self, userEmail, password):
+        self.userEmail = userEmail
+        self.password = password
+
+    def setSpeed(self, subject: Intersection, speed:int ):
+        return True
+    
+    def setSignalTiming(self, stoplight: int, pedestrianLight: int):
+        return None
+    
+    def setEmergencySignal(self, subject: EmergencyVehicle, emergencySignal: str):
+        return None
+    
+    def addVehicle(self, name: str, speed: int, type: str):
+        return None
+    
+    def addPedestrian(self, name: str):
+        return None
+    
+    def getCurrentData(self):
+        return [] #supposed to return an array
+    
+    def login(self, user: str, password: str):
+        return True
+    
+    def logout(self):
+        return None
