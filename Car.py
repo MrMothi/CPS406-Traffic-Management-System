6ffa6d8c913795be72f4b54df8db47510aebb680
MrMothi <90167278+MrMothi@users.noreply.github.com>
2023-04-03 03:02:49 -0400

    test1

--- Car.py
+++ Car.py
@@ -3,10 +3,8 @@ class Car(Vehicle):
     def __init__(self, operational, stopped, name, speed, type, plate,  inter, rd, actionType, carArrayNum, car_type):
         super().__init__(operational, stopped, name, speed, type, plate, inter, rd, actionType, carArrayNum)
         self.carType = car_type
-        # self.operational = operational
-        # self.inIntersection = inIntersection
-        # self.stopped = stopped
-        # self.name = name
-        # self.speed = speed
-        # self.type = type
-        # self.plate = plate
+
+
+    #temp code
+    def temp():
+        pass
